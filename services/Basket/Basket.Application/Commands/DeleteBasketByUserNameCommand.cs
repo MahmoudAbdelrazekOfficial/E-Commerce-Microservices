44a70fcf8fdb58ef80827c05609def7ed7d79b8c
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-01 18:31:52 +0200

    add Create and DeleteBasketByUserNameCommandHandler

---
+++ services/Basket/Basket.Application/Commands/DeleteBasketByUserNameCommand.cs
@@ -0,0 +1,19 @@
+﻿using Basket.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.Application.Commands
+{
+    public class DeleteBasketByUserNameCommand : IRequest<Unit> //Unit doesnt return any thing 
+    {
+        public string UserName { get; set; }
+        public DeleteBasketByUserNameCommand(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
