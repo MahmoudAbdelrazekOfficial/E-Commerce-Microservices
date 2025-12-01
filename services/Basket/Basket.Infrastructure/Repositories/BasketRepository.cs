c0d3bd61b8d4e8e92a0206bd96b92851c3df6d32
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-01 17:06:11 +0200

    create entities & Repositories

---
+++ services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,57 @@
+﻿using Basket.Core.Entities;
+using Basket.Core.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Basket.Infrastructure.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache; 
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (!string.IsNullOrEmpty(basket)) 
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
+        {
+            var basket = await _redisCache.GetStringAsync(cart.UserName);
+            if (basket!= null) 
+            {
+                //logic
+                return await GetBasket(cart.UserName);
+            }
+            else
+            {
+                await _redisCache.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+                return await GetBasket(cart.UserName);
+            }
+
+        }
+        public async Task DeleteBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (basket != null) 
+            {
+                await _redisCache.RemoveAsync(userName);
+            }
+
+        }
+
+    }
+}
