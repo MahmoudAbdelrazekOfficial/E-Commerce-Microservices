295700beeb1462cfdc31259af51446c3a1387af7
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-02 12:42:40 +0200

    add entity and contract repository of discount

---
+++ services/Discount/Discount.Core/Entities/Coupon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Core.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } // should be ProductId 
+        public string Description { get; set; }
+        public decimal DiscountAmount { get; set; }
+    }
+}
