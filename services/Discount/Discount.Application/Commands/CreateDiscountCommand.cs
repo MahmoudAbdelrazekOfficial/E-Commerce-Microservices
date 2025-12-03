cc169c35e8570f2ccd25da48cd0c6c993ca1300d
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-03 14:23:54 +0200

    change discount amount to double

--- services/Discount/Discount.Application/Commands/CreateDiscountCommand.cs
+++ services/Discount/Discount.Application/Commands/CreateDiscountCommand.cs
@@ -12,6 +12,6 @@ namespace Discount.Application.Commands
     {
         public string ProductName { get; set; }
         public string Description { get; set; }
-        public int DiscountAmount { get; set; }
+        public double DiscountAmount { get; set; }
     }
 }
