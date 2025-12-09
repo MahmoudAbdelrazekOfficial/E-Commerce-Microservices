8288c332222f4947fe8a616928526df70c2d992c
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-09 10:29:54 +0200

    add base repo

---
+++ services/Ordering/Ordering.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,43 @@
+﻿using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    {
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
