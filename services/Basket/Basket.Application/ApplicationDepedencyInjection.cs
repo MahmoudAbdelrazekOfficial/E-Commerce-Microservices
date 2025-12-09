001ff6b5ef5013628ecd32b6d06b19e5bc35dd6f
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-09 12:57:55 +0200

    change dir of services of automapper and MediatR to application dependency Injection

---
+++ services/Basket/Basket.Application/ApplicationDepedencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Basket.Application.Commands;
+using Basket.Application.Mappers;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Basket.Application;
+
+public static class ApplicationDepedencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(BasketMappingProfile).Assembly);
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly(),
+            Assembly.GetAssembly(typeof(CreateShoppingCartCommand))));
+
+        return services;
+    }
+}
+
