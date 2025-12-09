13de25c7aff930e1be8c600c7cb00f91c68b9635
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-09 12:13:19 +0200

    OrderContext and factory for order

---
+++ services/Ordering/Ordering.Infrastructure/Data/OrderContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextFactory : IDesignTimeDbContextFactory<OrderContext>
+    {
+        public OrderContext CreateDbContext(string[] args) 
+        {
+            var optionBuilder = new DbContextOptionsBuilder<OrderContext>();
+            optionBuilder.UseSqlServer("Server=localhost;Database=OrderDb2;User Id=sa;Password=password@123;TrustServerCertificate=True;");
+            return new OrderContext(optionBuilder.Options);
+        }
+
+    }
+}
