e6b064588519ba08ab963f55db03a5edf3b97df4
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-23 16:26:16 +0200

    create dbExtention and modify appSettings and launchSettings

---
+++ services/Ordering/Ordering.API/Extentions/DbExtention.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+namespace Ordering.API.Extentions
+{
+    public static class DbExtention
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation($"Started database migration : {typeof(TContext).Name}");
+                    var retry = Policy.Handle<SqlException>()
+                        .WaitAndRetry(
+                           retryCount:5,
+                           sleepDurationProvider:retryAttempts=>TimeSpan.FromSeconds(Math.Pow(2,retryAttempts)),
+                           onRetry:(exception,span,count) =>
+                           {
+                               logger.LogInformation($"retrying because of {exception} {span} ");
+                           }
+                        );
+                    retry.Execute(() => CallSeeder(seeder, context, services));
+                    logger.LogInformation($"finished database migration : {typeof(TContext).Name}");
+                }
+                catch (Exception ex)
+                {
+
+                    logger.LogInformation($"an error occured whill migration database : {typeof(TContext).Name} ");
+                }
+            }
+            return host;
+        }
+
+        private static void CallSeeder<TContext>(Action<TContext,IServiceProvider> seeder,TContext ? context ,IServiceProvider serviceProvider) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, serviceProvider);
+        }
+    }
+}
---
+++ services/Ordering/Ordering.Application/Extentions/ServiceRegisteration.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Extentions
+{
+    public static class ServiceRegisteration
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>),typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>),typeof(UnhandledExceptionBehavior<,>));
+
+            return services;
+        }
+    }
+}
