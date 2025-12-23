fa5d78037ecc91402a6604cebcf629ae907b6d7b
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-23 10:00:35 +0200

    CheckoutOrder

---
+++ services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Exceptions
+{
+    public class OrderNotFoundException : ApplicationException
+    {
+        public OrderNotFoundException(string name , Object Key):base($"Entity {name} - {Key} isn't found")
+        {             
+        }
+    }
+}
---
+++ services/Ordering/Ordering.Application/Validators/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Ordering.Application.Commands;
+
+namespace Ordering.Application.Validators
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CreateCheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(o => o.UserName)
+                .NotEmpty()
+                .WithMessage("{UserName} is required")
+                .NotNull()
+                .MaximumLength(70)
+                .WithMessage("User Name must not exceed 70 characters");
+
+            RuleFor(o => o.TotalPrice)
+                .NotEmpty()
+                .WithMessage("total price is required")
+                .GreaterThan(-1)
+                .WithMessage("Total price shouldn't be negative");
+
+            RuleFor(o => o.EmailAddress)
+                .NotEmpty()
+                .WithMessage("Email Address is required");
+
+            RuleFor(o => o.FirstName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("FirstName is required");
+
+            RuleFor(o => o.LastName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("LastName is required");
+        }
+    }
+}
