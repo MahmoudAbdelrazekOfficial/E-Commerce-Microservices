a9c14b9ecac14c6a13abfdefe57a38734d42861b
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-23 10:43:16 +0200

    create ValidationBehavior

---
+++ services/Ordering/Ordering.Application/Validators/UpdateCheckoutOrderCommandValidator.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using Ordering.Application.Commands;
+
+namespace Ordering.Application.Validators
+{
+    public class UpdateCheckoutOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateCheckoutOrderCommandValidator()
+        {
+            RuleFor(o => o.Id)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("{Id} is required")
+                .GreaterThan(0)
+                .WithMessage("{Id} cann't be negative");
+
+            RuleFor(o => o.UserName)
+                .NotEmpty()
+                .WithMessage("{UserName} is required")
+                .NotNull()
+                .MaximumLength(70)
+                .WithMessage("User Name must not exceed 70 characters");
+
+            RuleFor(o => o.TotalPrice)
+                .NotEmpty()
+                .WithMessage("total price is required")
+                .GreaterThan(-1)
+                .WithMessage("Total price shouldn't be negative");
+
+            RuleFor(o => o.EmailAddress)
+                .NotEmpty()
+                .WithMessage("Email Address is required");
+
+            RuleFor(o => o.FirstName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("FirstName is required");
+
+            RuleFor(o => o.LastName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("LastName is required");
+        }
+    }
+}
