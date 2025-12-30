5b8f80d5dcee10263a60cfcfb28a7e4ccdb8a13b
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2025-12-30 16:40:04 +0200

    MassTrasient with RabbitMQ

---
+++ Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class BaseIntegrationEvent
+    {
+        public string CorrelationId { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public BaseIntegrationEvent()
+        {
+            CorrelationId = Guid.NewGuid().ToString();
+            CreationDate = DateTime.Now;
+        }
+        public BaseIntegrationEvent(Guid corrlationId , DateTime creationDate)
+        {
+            CorrelationId = corrlationId.ToString();
+            CreationDate = creationDate;
+        }
+    }
+}
