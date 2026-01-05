cfea17d64e0ee7c51c97ab8cb9ccc22084700335
MahmoudAbdelrazek <Mahmoud@DESKTOP-2SVMNBN>
2026-01-05 10:51:27 +0200

    add Configuration for logging with serilog

---
+++ Infrastructure/Logging/Common.Logging/Logging.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+
+namespace Common.Logging
+{
+    public static class Logging
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogger => (context, loggerConfiguration) =>
+        {
+            var env = context.HostingEnvironment;
+
+            loggerConfiguration.MinimumLevel.Information()
+            .Enrich.FromLogContext() // CorrelationId
+            .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+            .Enrich.WithProperty("Environment", env.EnvironmentName)
+            .Enrich.WithExceptionDetails()
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.Hosting.LifeTime", LogEventLevel.Warning)
+            .WriteTo.Console();
+
+            //for development 
+            if (context.HostingEnvironment.IsDevelopment())
+            {
+                loggerConfiguration.MinimumLevel.Override("Catalog", LogEventLevel.Debug);
+                loggerConfiguration.MinimumLevel.Override("Basket", LogEventLevel.Debug);
+                loggerConfiguration.MinimumLevel.Override("Discount", LogEventLevel.Debug);
+                loggerConfiguration.MinimumLevel.Override("Ordering", LogEventLevel.Debug);
+                
+            }
+            
+
+        };
+     }
+}
